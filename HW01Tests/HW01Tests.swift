c3be3780fd9ae5ffb1d7b5d0deaba7255abde4df
LaurenceLin <laurencelin0614@icloud.com>
2024-11-19 16:10:23 +0800

    Initial Commit

---
+++ HW01Tests/HW01Tests.swift
@@ -0,0 +1,17 @@
+//
+//  HW01Tests.swift
+//  HW01Tests
+//
+//  Created by 林上閔 on 2024/11/19.
+//
+
+import Testing
+@testable import HW01
+
+struct HW01Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
